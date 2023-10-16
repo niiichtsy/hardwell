f4f8c9eb04f2a50d96e6f86929ba1e2681e3f412
niiichtsy <niiichtsy@gmail.com>
2023-10-16 20:08:57 +0200

    Added testbench to clk_divider

---
+++ clk_divider/src/clk_divider.v
@@ -0,0 +1,31 @@
+module clk_divider #(
+  parameter DIV_VALUE = 125000000,
+  parameter DIV_WIDTH = 32
+) (
+    output reg clk_out,
+    input clk_in,
+    input resetn
+);
+  reg [DIV_WIDTH-1:0] div_counter;
+
+  always @(posedge clk_in) begin
+    if (!resetn) begin
+      div_counter <= 'h00;
+      clk_out <= 1'b0;
+    end else begin
+
+      if (div_counter == DIV_VALUE) begin
+        div_counter <= 'h00;
+        clk_out <= ~clk_out;
+      end else div_counter <= div_counter + 1'b1;
+
+    end
+  end
+
+  // Testbench waves
+  initial begin
+    $dumpvars(0, clk_divider);
+    $dumpfile("dump.vcd");
+  end
+
+endmodule
