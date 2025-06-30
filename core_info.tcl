b9344d72395c78f102097bdf2b031e508f6224cd
niiichtsy <niiichtsy@gmail.com>
2025-06-30 09:32:37 +0200

    Removed unnecessary temp file

--- core_info.tcl
+++
@@ -1,7 +0,0 @@
-# IP Core identification and description 
-
-set info_ipCoreName "Adder-Subtractor"
-set info_ipCoreVersion $env(VERSION)
-set info_ipCoreRevision $env(REVISION)
-set info_ipCoreDescription "Adding and subtracting circuit"
-set info_ipCoreDisplayName "add_sub"
